d1fb49c9f0cf01282d115349074e1d2b6aeb1fdb
vwolfley <vern.wolfley@gmail.com>
2021-11-08 21:10:26 -0700

    added google anylitics

--- template/src/App.js
+++ template/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactGA from "react-ga";
 import { MainMap } from "./components/Map";
 
 import "@arcgis/core/assets/esri/themes/light/main.css";
@@ -9,6 +10,13 @@ import "./styles/App.scss";
 import LogoWidget from "./components/Widgets/logoWidget";
 
 function App() {
+  const TRACKING_ID = "UA-29422512-1";
+  ReactGA.initialize(TRACKING_ID, {
+    debug: true,
+    titleCase: false,
+    gaOptions: {},
+  });
+
   return (
     <>
       <div className="app-container">
