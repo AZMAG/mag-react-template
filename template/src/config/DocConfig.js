0f8d288743fa4e09797568890e61e141b3b2b8e9
vwolfley <vern.wolfley@gmail.com>
2023-04-10 11:28:49 -0700

    add to docconfig"

--- template/src/config/DocConfig.js
+++ template/src/config/DocConfig.js
@@ -2,12 +2,12 @@ let docConfig = {
   version: "v0.0.0",
   date: "2021-01-01",
   copyright: "2023",
+  MEASUREMENT_ID: "G-B129P7KDMQ",
   magLink: "https://www.azmag.gov/",
   mainURL: "",
 
   esriConfig_apiKey:
     "AAPK8e536e7d99f846a19a536120498f4084z4MVu8qEG5pH2biM3pGo6KTIUqNpE1snb8uF6IPClKy_3fxTumPIvHXw52ltzMFX",
 }
-};
 
-export default docConfig;
+export default docConfig
--- template/src/index.js
+++ template/src/index.js
@@ -2,9 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import ReactGA from "react-ga4"
 import App from "./App"
+import DocConfig from "./config/DocConfig"
 import "./index.css"
 
-ReactGA.initialize("G-B129P7KDMQ", { debug: true })
+ReactGA.initialize(DocConfig.MEASUREMENT_ID, { debug: true })
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
